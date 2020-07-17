235e67e29965ecc00d4b584b16f1273655499d79
李明鑫 <lmx_190133124@126.com>
2020-07-17 10:09:50 +0800

    删除无用文件

--- docker/dubbo/entrypoint/Java_Configuration.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# 检查目标环境
-sed -n "s/jdk.tls.disabledAlgorithms/\#jdk.tls.disabledAlgorithms/"p ${JAVA_HOME}/jre/lib/security/java.security
-sed -i "s/jdk.tls.disabledAlgorithms/\#jdk.tls.disabledAlgorithms/" ${JAVA_HOME}/jre/lib/security/java.security
-echo "调整Java配置成功";
